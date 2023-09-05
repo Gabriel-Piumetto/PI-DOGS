3d633bed2488d1715a6d6d165a03a84da9815fbe
Gabriel Piumetto <gabo125@gmail.com>
2023-09-05 14:36:22 -0300

    funciona tambien  get /dogs  y /dogs?=name

--- api/src/controllers/dogsController.js
+++ api/src/controllers/dogsController.js
@@ -6,10 +6,52 @@ const {Dog} = require('../db.js')
 const linkApi = `https://api.thedogapi.com/v1/breeds?api_key=${YOUR_API_KEY}`
 
 
-const createDog = async (image, name, height, weight, life_span)=>{
-await Dog.create({image, name, height, weight, life_span})
-}
+
+const getDogs = async ()=>{
+    
+    const response = await axios.get(linkApi);
+    const rawDataApi = response.data;
+
+    const dogsApi = rawDataApi.map((obj) => obj);
+
+    const resultDb = await Dog.findAll();
+
+    const dogsDb = resultDb.map((obj) => obj.dataValues);
+
+    const allDogs = dogsDb.concat(dogsApi);
+
+    return allDogs
+    
+}    
+
+module.exports={getDogs}
+
+
+
+       
+    // const getDogsApi = async ()=>{
+    //     
+    
+    
+  
+    
+// }
+
+
+// const getDogsDb = async ()=>{
+ 
+
+
+
+
+// }
+
+
+
+// const createDog = async (image, name, height, weight, life_span)=>{
+// await Dog.create({image, name, height, weight, life_span})
+// 
+
 
     
     
-module.exports={createDog}
--- api/src/handlers/dogsHandler.js
+++ api/src/handlers/dogsHandler.js
@@ -1,13 +1,34 @@
-const { createDog } = require("../controllers/dogsController")
+const { getDogs, createDog } = require("../controllers/dogsController")
 
 
-const getAllDogs = (req, res)=>{
 
-
-    if(req.query.name){
-        res.status(200).send(`NIY todas las razas q coinciden con${req.query.name}` )    
-    }
-    res.status(200).send("NIY arr de obj de todos los breeds")
+const getAllDogs = async (req, res)=>{
+    
+    try {
+        if (req.query.name) {
+          let dogsRevisados = [];
+          const { name } = req.query;
+    
+          let dogsSinRevisar = await getDogs();
+    
+          dogsSinRevisar.forEach((dog) => {
+            if (dog.name.toLowerCase().includes(name.toLowerCase())) {
+              dogsRevisados.push(dog);
+            }
+          });
+    
+          if (dogsRevisados.length !== 0) {
+            console.log(dogsRevisados);
+            return res.status(200).json(dogsRevisados);
+          } else {
+            throw new Error(`No se encuentra la raza ${name}`);
+          }
+        }
+    
+        res.status(200).json(await getDogs());
+      } catch (error) {
+        res.status(400).json({ error: error.message });
+      }
 
 }
 
