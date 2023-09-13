6ae607f034b970338df7c5b8a7d78ca2fc0775fa
Gabriel Piumetto <gabo125@gmail.com>
2023-09-13 08:01:54 -0300

    landing page funciona

--- client/src/views/Landing/Landing.jsx
+++ client/src/views/Landing/Landing.jsx
@@ -1,10 +1,23 @@
-const Landing =()=>{
+import style from './Landing.module.css'
+import { Link } from "react-router-dom"
+
+const image='assets/dogs-img.jpg'
+
+
+const Landing = ()=> {
 
     return (
+        
         <div>
-            <h1>Esta es la vista de Langing</h1>
+            <img alt='Dogs Henry' className={style.image} src={image}></img>
+
+
+            <button className={style.button}><Link to="/home">ENTRAR</Link></button>
+        
         </div>
+
     )
+    
 }
 
 export default Landing
